a74f154c4ce34e6b3d32ddde8e1ea8ca525b6417
Ariel <arielwwl03@gmail.com>
2024-12-02 19:53:32 +0100

    Update Reports.py

--- 2024/Day2/Reports.py
+++ 2024/Day2/Reports.py
@@ -9,9 +9,16 @@ def safecheck(x):
     diff = [x[i]-x[i+1] for i in range(len(x)-1)] # calculates and shows the difference between 2 numbers in the list.
     if (max(diff) <= 3 and min(diff) >= 1) or (max(diff) <= -1 and min(diff) >= -3): # only returns true for lists that have difference between 1 and 3.
         return True
-    else:
-        return False
+    return False
+    
+def dampener(x):
+    for i in range(len(x)):
+        newlvl = x[:i] + x[i+1:] # creates new lists of every possibility of a report with one level dropped.
+        if safecheck(newlvl):
+            return True
+    return False
 
 print(sum([safecheck(i) for i in reports])) # part 1. Sums number of trues by iterating through all the reports.
 
+print(sum([dampener(i) for i in reports])) # part 2
 
